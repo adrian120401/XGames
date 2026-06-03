31200df6869ec6c6f3e264e25e96e366faf127ba
P14XO <123851525+P14XO@users.noreply.github.com>
2026-06-03 20:47:58 -0300

    main menu body

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,21 @@ function App() {
 
 
   return (
+<>
+<div>
+  {/*NavBar */}
+</div>
 
+<div className='heroSection'>
+  <h6>Sobre el proyecto</h6>
+{/*Hero section here */}
+</div>
+<div>
+  <h6>Juegos destacados</h6>
+  {/*Carrousel here */}
+</div>
+
+</>
   )
 }
 
