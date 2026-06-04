cc82918736438fc8b10a7e9e1e1b315a8ed88bca
Agusito <agustin.bech1@gmail.com>
2026-06-03 21:24:44 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-
   return (
-<>
-<div>
-  {/*NavBar */}
-</div>
+    <>
+      <div>{/*NavBar */}</div>
 
-<div className='heroSection'>
-  <h6>Sobre el proyecto</h6>
-{/*Hero section here */}
-</div>
-<div>
-  <h6>Juegos destacados</h6>
-  {/*Carrousel here */}
-</div>
-
-</>
-  )
+      <div className="heroSection">
+        <h6>Sobre el proyecto</h6>
+        {/*Hero section here */}
+      </div>
+      <div>
+        <h6>Juegos destacados</h6>
+        {/*Carrousel here */}
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
