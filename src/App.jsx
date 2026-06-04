261605c28183dd82d41942497477400f6f860b3f
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2026-06-04 18:36:15 -0300

    build: Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
 
 function App() {
+    return (
+        <>
+            <div>{/*NavBar */}</div>
 
-
-  return (
-<>
-<div>
-  {/*NavBar */}
-</div>
-
-<div className='heroSection'>
-  <h6>Sobre el proyecto</h6>
-{/*Hero section here */}
-</div>
-<div>
-  <h6>Juegos destacados</h6>
-  {/*Carrousel here */}
-</div>
-
-</>
-  )
+            <div className="heroSection">
+                <h6>Sobre el proyecto</h6>
+                {/*Hero section here */}
+            </div>
+            <div>
+                <h6>Juegos destacados</h6>
+                {/*Carrousel here */}
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
