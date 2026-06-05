738d9d584422ce947dcea3d865cfb1f3a82d6c16
P14XO <123851525+P14XO@users.noreply.github.com>
2026-06-05 19:29:20 -0300

    expedition33 page

---
+++ src/pages/Expedition33.jsx
@@ -0,0 +1,17 @@
+import Game from '../components/Game';
+
+export default function Expedition33() {
+    return (
+        <Game
+            title="Expedition 33"
+            youtubeUrl="https://youtu.be/9baCqcVrNCc?si=MRHQ4Da33MlTomGN"
+            coverImage="https://image.api.playstation.com/vulcan/ap/rnd/202501/2217/e5833a53529ff9879e87689f1e8b04d45ba7e6c97fa791e2.png"
+            summary="Una vez al año, la Pintora se despierta y pinta sobre el Monolito su número maldito. Toda persona cuya edad sea mayor que ese número se convierte en humo y desaparece. Año tras año, ese número desciende y más personas se esfuman. Mañana, se despertará y pintará un 33, y emprenderemos nuestra misión final: acabar con la Pintora para que nunca más pueda pintar la muerte."
+            developer="Eric Gallo"
+            price="$1350"
+            description="Explora un reino cautivador habitado por enemigos fantásticos. Recorre escenarios increíbles, desde la isla de Visages hasta el Campo de Batalla Olvidado, mientras encuentras secretos y misiones ocultas en el camino. Descubre aliados de gran ayuda en criaturas legendarias y recluta a compañeros especiales, accede a nuevas formas de viajar y localiza zonas secretas en el mapa del mundo.
+
+Sumérgete en el juego debut de Sandfall Interactive, creado con Unreal Engine 5, que combina unos gráficos impresionantes y una banda sonora conmovedora."
+        />
+    );
+}
